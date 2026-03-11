9066e5226c3559231c31a5ecc142d38e82d79bd0
igorBoving <igorjoakinsonboving@gmail.com>
2026-03-11 10:29:27 -0300

    Update layout.py

--- layout.py
+++ layout.py
@@ -1,21 +1,21 @@
 import matplotlib.pyplot as plt
 
-def desenhar_chapa(packer, largura_chapa, altura_chapa):
+def desenhar_chapa(packer, largura_chapa, altura_chapa, indice_chapa=0):
 
     fig, ax = plt.subplots()
 
-    for abin in packer:
+    abin = packer[indice_chapa]
 
-        for rect in abin:
+    for rect in abin:
 
-            x = rect.x
-            y = rect.y
-            w = rect.width
-            h = rect.height
+        x = rect.x
+        y = rect.y
+        w = rect.width
+        h = rect.height
 
-            r = plt.Rectangle((x,y), w, h, fill=False)
+        r = plt.Rectangle((x, y), w, h, fill=False)
 
-            ax.add_patch(r)
+        ax.add_patch(r)
 
     ax.set_xlim(0, largura_chapa)
     ax.set_ylim(0, altura_chapa)
