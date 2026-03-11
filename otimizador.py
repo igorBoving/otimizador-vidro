726fb8c4c4ae5ade848334612a1d8536a33f5c7d
igorBoving <igorjoakinsonboving@gmail.com>
2026-03-11 10:43:38 -0300

    Update otimizador.py

--- otimizador.py
+++ otimizador.py
@@ -5,7 +5,7 @@ def otimizar_corte(df, largura_chapa, altura_chapa):
     packer = newPacker(rotation=True)
 
     for _, r in df.iterrows():
-        packer.add_rect(r["largura"], r["altura"])
+        packer.add_rect(r["largura"], r["altura"], rid=r["codigo"])
 
     packer.add_bin(largura_chapa, altura_chapa, float("inf"))
 
