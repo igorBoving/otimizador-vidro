97a6ba817709733203e2d03a708aa768cf67e6c0
igorBoving <igorjoakinsonboving@gmail.com>
2026-03-11 10:44:36 -0300

    Update historico.py

--- historico.py
+++ historico.py
@@ -1,28 +1,21 @@
 import pandas as pd
 import os
 
-arquivo = "historico_producao.csv"
+arquivo = "historico.csv"
 
-def salvar_producao(dados):
+def salvar_hist(dados):
 
     if os.path.exists(arquivo):
-
         df = pd.read_csv(arquivo)
-
         df = pd.concat([df, pd.DataFrame([dados])])
-
     else:
-
         df = pd.DataFrame([dados])
 
     df.to_csv(arquivo, index=False)
 
-def carregar_historico():
+def carregar_hist():
 
     if os.path.exists(arquivo):
-
         return pd.read_csv(arquivo)
 
-    else:
-
-        return pd.DataFrame()
+    return pd.DataFrame()
